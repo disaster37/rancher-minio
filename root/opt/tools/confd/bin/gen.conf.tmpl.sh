92cee9f73d2b04d661cdd74b1e37364b1e170253
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-06 17:54:01 +0100

    Fix confd template

--- root/opt/tools/confd/bin/gen.conf.tmpl.sh
+++ root/opt/tools/confd/bin/gen.conf.tmpl.sh
@@ -19,5 +19,5 @@ EOF
 
 cat << EOF > ${SERVICE_VOLUME}/confd/etc/templates/minio-server.cfg.tmpl
 MINIO_DATA=${MINIO_DATA}
-MINIO_VOLUMES="{{range \$i, \$containerName := ls \"/containers\"}}http://{{getv (printf \"/containers/%s/primary_ip\" \$containerName)}}${MINIO_DATA} {{end}}"
+MINIO_VOLUMES="{{range \$i, \$containerName := ls "/containers"}}http://{{getv (printf "/containers/%s/primary_ip" \$containerName)}}${MINIO_DATA} {{end}}"
 EOF
