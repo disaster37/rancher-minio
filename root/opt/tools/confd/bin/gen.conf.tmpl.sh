4ae12c471dd5027397290d73fd62a87eb614c6a3
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-07 10:16:25 +0100

    Try to count number of server

--- root/opt/tools/confd/bin/gen.conf.tmpl.sh
+++ root/opt/tools/confd/bin/gen.conf.tmpl.sh
@@ -19,6 +19,6 @@ EOF
 
 cat << EOF > ${SERVICE_VOLUME}/confd/etc/templates/minio-server.cfg.tmpl
 MINIO_DATA=${MINIO_DATA}
-MINIO_NUMBER_SERVERS= "{{range \$i := ls "/containers"}}(printf "%s" \$i){{end}}"
+MINIO_NUMBER_SERVERS= "{{range \$i := ls "/containers"}}{{(printf "%s" \$i)}}{{end}}"
 MINIO_VOLUMES="{{range \$i, \$containerName := ls "/containers"}}http://{{getv (printf "/containers/%s/primary_ip" \$containerName)}}${MINIO_DATA}/export {{end}}"
 EOF
