bd310e6723be483ad0d9eb2c6c790c5cd2cb7a49
Sebastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2017-02-07 16:53:30 +0100

    Fix start script

--- root/opt/tools/confd/bin/service-conf-scheduler.sh
+++ root/opt/tools/confd/bin/service-conf-scheduler.sh
@@ -33,12 +33,12 @@ function serviceStart {
     checkNetwork
     serviceTemplate
     log "[ Starting ${CONF_NAME}... ]"
-    start-stop-daemon --background --name ${CONF_NAME} --start --quiet --stdout ${SERVICE_HOME}/logs/${CONF_NAME}.log --stderr ${SERVICE_HOME}/logs/${CONF_NAME}.log --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_INTERVAL}
+    start-stop-daemon --background --name ${CONF_NAME} --start --quiet --stdout ${SERVICE_HOME}/logs/${CONF_NAME}.log --stderr ${SERVICE_HOME}/logs/${CONF_NAME}.log --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_BIN} -- ${CONF_OPTS}
 }
 
 function serviceStop {
     log "[ Stoping ${CONF_NAME}... ]"
-    start-stop-daemon --name ${CONF_NAME} --retry 5 --oknodo --stop --quiet --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_INTERVAL}  > /dev/null 2>&1
+    start-stop-daemon --name ${CONF_NAME} --retry 5 --oknodo --stop --quiet --pidfile ${SERVICE_HOME}/${CONF_NAME}.pid --exec ${CONF_BIN}  > /dev/null 2>&1
 }
 
 function serviceRestart {
@@ -57,7 +57,7 @@ CONF_PREFIX=${CONF_PREFIX:-"/2015-12-19"}
 CONF_INTERVAL=${CONF_INTERVAL:-60}
 CONF_PARAMS=${CONF_PARAMS:-"-confdir /opt/tools/confd/etc -backend ${CONF_BACKEND} -prefix ${CONF_PREFIX}"}
 CONF_ONETIME="${CONF_BIN} -onetime ${CONF_PARAMS}"
-CONF_INTERVAL="${CONF_BIN} -interval ${CONF_INTERVAL} ${CONF_PARAMS}"
+CONF_OPTS="-interval ${CONF_INTERVAL} ${CONF_PARAMS}"
 
 case "$1" in
         "start")
